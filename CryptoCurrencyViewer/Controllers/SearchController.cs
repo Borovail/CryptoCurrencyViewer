94513f57c2bff48cb789fb414808cff79fd5804a
Borovail <skorikdaniil17@gmail.com>
2023-10-29 20:49:33 +0200

    page : Search implemented : crypto search

--- CryptoCurrencyViewer/Controllers/SearchController.cs
+++ CryptoCurrencyViewer/Controllers/SearchController.cs
@@ -1,9 +1,11 @@
 ﻿using CryptoCurrencyViewer.Interfaces;
 using CryptoCurrencyViewer.Models;
+using CryptoCurrencyViewer.Models.MainPagesModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CryptoCurrencyViewer.Controllers
 {
+    [Controller]
     public class SearchController : Controller
     {
         private readonly IApiService _apiService;
@@ -13,11 +15,21 @@ namespace CryptoCurrencyViewer.Controllers
             _apiService = cryptoService;
         }
 
+
+        
         public async Task< IActionResult> Search()
         {
             var btc = await _apiService.GetCryptoInfoByNameAsync("bitcoin");
 
             return View(btc);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> SearchCrypto([FromBody] CryptoRequestModel selectedCrypto)
+        {
+            var crypto = await _apiService.GetCryptoInfoByNameAsync(selectedCrypto.selectedCrypto);
+
+            return Json(crypto);
         }
     }
 }
--- CryptoCurrencyViewer/Services/ApiService.cs
+++ CryptoCurrencyViewer/Services/ApiService.cs
@@ -1,6 +1,7 @@
 ﻿using CryptoCurrencyViewer.Interfaces;
 using CryptoCurrencyViewer.Models;
 using Newtonsoft.Json.Linq;
+using System.ComponentModel;
 
 namespace CryptoCurrencyViewer.Services
 {
@@ -21,17 +22,36 @@ namespace CryptoCurrencyViewer.Services
                 JObject cryptoData = JObject.Parse(responseData);
 
                 string name = cryptoData["name"]?.ToString();
-                string symbol = cryptoData?["symbol"]?.ToString().ToUpper();
-                double currentPrice = Convert.ToDouble(cryptoData["market_data"]["current_price"]["usd"]);
-                string imageUrl = cryptoData["image"]["large"].ToString();
-                double marketCap = Convert.ToDouble(cryptoData["market_data"]["market_cap"]["usd"]);   
-                double priceChangePercentage24h = Convert.ToDouble(cryptoData["market_data"]["price_change_percentage_24h"]);
-                double volume24h = Convert.ToDouble(cryptoData["market_data"]["total_volume"]["usd"]);
-                double high24h = Convert.ToDouble(cryptoData["market_data"]["high_24h"]["usd"]);
-                double low24h = Convert.ToDouble(cryptoData["market_data"]["low_24h"]["usd"]);
-                double priceChangePercentage7d = Convert.ToDouble(cryptoData["market_data"]["price_change_percentage_7d"]);
-                double totalSupply = Convert.ToDouble(cryptoData["market_data"]["total_supply"]);
-                double maxSupply = Convert.ToDouble(cryptoData["market_data"]["max_supply"]);
+                string symbol = cryptoData["symbol"]?.ToString().ToUpper();
+                string imageUrl = cryptoData["image"]["large"]?.ToString();
+
+                double currentPrice;
+                double.TryParse(cryptoData["market_data"]["current_price"]["usd"]?.ToString(), out currentPrice);
+
+                double marketCap;
+                double.TryParse(cryptoData["market_data"]["market_cap"]["usd"]?.ToString(), out marketCap);
+
+                double priceChangePercentage24h;
+                double.TryParse(cryptoData["market_data"]["price_change_percentage_24h"]?.ToString(), out priceChangePercentage24h);
+
+                double volume24h;
+                double.TryParse(cryptoData["market_data"]["total_volume"]["usd"]?.ToString(), out volume24h);
+
+                double high24h;
+                double.TryParse(cryptoData["market_data"]["high_24h"]["usd"]?.ToString(), out high24h);
+
+                double low24h;
+                double.TryParse(cryptoData["market_data"]["low_24h"]["usd"]?.ToString(), out low24h);
+
+                double priceChangePercentage7d;
+                double.TryParse(cryptoData["market_data"]["price_change_percentage_7d"]?.ToString(), out priceChangePercentage7d);
+
+                double totalSupply;
+                double.TryParse(cryptoData["market_data"]["total_supply"]?.ToString(), out totalSupply);
+
+                double maxSupply;
+                double.TryParse(cryptoData["market_data"]["max_supply"]?.ToString(), out maxSupply);
+
 
                 return new CryptoModel(
                     name, 
